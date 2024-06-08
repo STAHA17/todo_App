974158149636ab1c25f51f47e14399383678bb02
STAHA17 <tali171717@gmail.com>
2024-06-08 22:34:01 +0500

    modify

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\SubtaskController;
 
 Route::resource('tasks', TaskController::class);
-    
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('tasks.subtasks', SubtaskController::class);
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::put('/tasks/{taskId}/subtasks/{subtaskId}', [SubtaskController::class, 'update'])->name('subtasks.update');
+
+
+Auth::routes();
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
